8fe59912285a3413024467d875da86d09d86ad25
SBoschCladera <73190236+SBoschCladera@users.noreply.github.com>
2022-10-27 18:50:25 +0200

    Creada nueva clase Juego; ajustes para la clase
    Nueva clase Juego para las variables de entrada del usuario mediante prompt

--- Actividades/ProyectoMemory/js/memory.js
+++ Actividades/ProyectoMemory/js/memory.js
@@ -1,8 +1,3 @@
-// Declaración de variables
-let maxfilas = 6;
-let maxColumnas = 12;
-
-
 // Crea la clase Tablero
 class Tablero {
     constructor(filas, columnas) {
@@ -22,27 +17,26 @@ class Tablero {
                 this.tablero[i][j] = "o";
             }
         }
-        return this.tablero;
+        this.tablero;
     }
 }
 
-// Clase Memory hereda de la clase Tablero
-class Memory extends Tablero {
-    constructor(filas, columnas, tematicaPareja) {
-        super(filas, columnas);
-        this.tematicaPareja = tematicaPareja;
+// Crea la clase Juego
+class Juego {
+    constructor() {
+        this.maxFilas = 6;
+        this.maxColumnas = 12;
 
-        this.iniciarApp();
-        // this.arrayDeParejas();
-        this.desordenarArray();
-        // this.colocarParejas();
-        this.pintarTableroEnPantalla();
+        this.pedirDatosUsuario();
     }
 
-    // Inicia la aplicación solicitando el número de filas y columnas al usuario, realiza comprobaciones para saber si el número de casillas del tablero es correcto (debe ser un número par),
-    // debe contener un mínimo de 2 parejas y finalmente pinta el tablero en pantalla.
-    iniciarApp() {
-        this.pedirDatosUsuario();
+    // Solicita al usuario el número de filas, columnas y la temática de las parejas para el tablero, cumpliendo unas ciertas condiciones.
+    pedirDatosUsuario() {
+
+        // Solicita número de filas y columas mediante prompt.
+        this.inputDatoUsuario();
+
+        // Comprueba si el número de casillas del tablero es correcto (debe ser un número par) y debe contener un mínimo de 2 parejas.
         let numParejas = parseInt((this.filas * this.columnas) / 2);
 
         while ((this.filas * this.columnas) % 2 != 0 || numParejas < 2) {
@@ -52,77 +46,94 @@ class Memory extends Tablero {
             } else if (numParejas < 2) {
                 alert('El número mínimo de parejas debe ser 2.')
             }
-            this.pedirDatosUsuario();
+            this.inputDatoUsuario();
+
             numParejas = parseInt((this.filas * this.columnas) / 2);
         }
-        this.pedirTematicaParejas();
+
+        // Aquí solicita la temática de las parejas
+        this.inputTematicaParejas();
+
+        // Inicia el Memory.
+        this.iniciarMemory()
+
     }
 
+    // Solicita número de filas y columas al usuario, éstas deben cumplir una serie de condiciones.
+    inputDatoUsuario() {
 
-    // Solicita al usuario el número de filas y columnas para el tablero, cumpliendo unas ciertas condiciones
-    pedirDatosUsuario() {
-
-        // Variables
-        let mensajeFila = 'Número de filas: ';
-        let mensajeColumna = 'Número de columnas: ';
-        let mensajeAlert1 = 'Debes introducir un valor númerico positivo.'
-        let mensajeAlert2 = 'El número máximo de filas es ' + maxfilas + '.'
-        let mensajeAlert3 = 'El número máximo de columnas es ' + maxColumnas + '.'
-
-        this.filas = prompt(mensajeFila);
+        this.filas = prompt('Número de filas del tablero: ');
 
         // Personaliza el mensaje de salida para las filas según el error detectado.
-        while (!Number(this.filas) || this.filas < 1 || this.filas > maxfilas) {
-
-            if (this.filas < 1 || !Number(this.filas)) {
-                alert(mensajeAlert1);
-            } else
-                if (this.filas > maxfilas) {
-                    alert(mensajeAlert2);
-                }
-            this.filas = prompt(mensajeFila);
+        while (!Number(this.filas) || this.filas < 1 || this.filas > this.maxFilas) {
+            this.condicionesDatoInput(this.filas, this.maxFilas, 'filas');
+            this.filas = prompt('Número de filas del tablero: ');
         }
 
-        this.columnas = prompt(mensajeColumna);
-
+        this.columnas = prompt('Número de columnas del tablero: ');
         // ... y para las columnas.
-        while (!Number(this.columnas) || this.columnas < 1 || this.columnas > maxColumnas) {
-
-            if (this.columnas < 1 || !Number(this.columnas)) {
-                alert(mensajeAlert1);
-            } else
-                if (this.columnas > maxColumnas) {
-                    alert(mensajeAlert3);
-                }
-            this.columnas = prompt(mensajeColumna);
+        while (!Number(this.columnas) || this.columnas < 1 || this.columnas > this.maxColumnas) {
+            this.condicionesDatoInput(this.columnas, this.maxColumnas, 'columnas');
+            this.columnas = prompt('Número de columnas del tablero: ');
         }
     }
 
-    // Solicita al usuario la temática que desea entre las dos opciones disponibles
-    pedirTematicaParejas() {
+
+    // Comprueba si se cumplen ciertas condiciones, personalizando el mensaje de salida.
+    condicionesDatoInput(filaColumna, maxFilasColumnas, texto) {
+
+        if (filaColumna < 1 || !Number(filaColumna)) {
+            alert('Debes introducir un valor númerico positivo.');
+        } else
+        if (filaColumna > maxFilasColumnas) {
+            alert('El número máximo de ' + texto + ' es ' + maxFilasColumnas + '.');
+        }
+    }
+
+    // Solicita al usuario la temática que desea entre las dos opciones disponibles.
+    inputTematicaParejas() {
 
         let mensaje = 'Elige una temática para el juego: \n1) Animales \n2) Halloween'
         let mensajeAlert = 'Valor incorrecto, prueba otra vez.'
         let expresionRegular1 = /animales/i;
         let expresionRegular2 = /halloween/i;
-        let expresionRegular3 =  /^[1-2]$/;
+        let expresionRegular3 = /^[1-2]$/;
 
         this.tematicaPareja = prompt(mensaje);
 
+        // Condiciones para la entrada de datos.
         while (!(expresionRegular1.test(this.tematicaPareja) || expresionRegular2.test(this.tematicaPareja) || expresionRegular3.test(this.tematicaPareja))) {
             alert(mensajeAlert)
             this.tematicaPareja = prompt(mensaje);
         }
-        this.arrayDeParejas();
+    }
+
+
+    iniciarMemory() {
+        const memory = new Memory(this.filas, this.columnas, this.tematicaPareja);
+    }
+}
+
+
+// Clase Memory hereda de la clase Tablero
+class Memory extends Tablero {
+    constructor(filas, columnas, tematicaPareja) {
+        super(filas, columnas);
+        this.tematicaPareja = tematicaPareja;
+        this.filas = filas;
+        this.columnas = columnas;
+
+        this.pintarTableroEnPantalla();
     }
 
     // Rellena un array con las imágenes por duplicado (par crear parejas) contenidas en el directorio imagen, éstas pueden provenir de 
     // dos directorios diferentes de forma aleatoria para dos temáticas diferentes. Devuelve un array con el número de parejas solicitadas 
-    // por el usuario colocadas en posiciones aleatorias.
+    // por el usuario colocadas en posiciones aleatorias. Las 10 primeras parejas siempre serán diferentes, comienzan a repetirse a partir
+    // de la pareja 11. 
     arrayDeParejas() {
 
         let numParejas = (this.filas * this.columnas) / 2;
-        this.arrayParejas = [];
+        const arrayParejas = [];
 
         for (let i = 0; i < numParejas; i++) {
 
@@ -136,19 +147,23 @@ class Memory extends Tablero {
             // Cuando el número de parejas es mayor de 10, rellena con valores aleatorios hasta llegar al número de parejas necesarias.
             if (i > 10) {
                 let parejaAleatoria = parseInt(Math.random() * 10);
-                this.arrayParejas.push('<img src="imagenes/' + this.tematicaPareja + '/' + parejaAleatoria + '.png" alt="imagen' + i + '"></img>');
-                this.arrayParejas.push('<img src="imagenes/' + this.tematicaPareja + '/' + parejaAleatoria + '.png" alt="imagen' + i + '"></img>');
+                arrayParejas.push('<img src="imagenes/' + this.tematicaPareja + '/' + parejaAleatoria + '.png" alt="imagen' + i + '"></img>');
+                arrayParejas.push('<img src="imagenes/' + this.tematicaPareja + '/' + parejaAleatoria + '.png" alt="imagen' + i + '"></img>');
             } else {
-                this.arrayParejas.push('<img src="imagenes/' + this.tematicaPareja + '/' + i + '.png" alt="imagen' + i + '"></img>');
-                this.arrayParejas.push('<img src="imagenes/' + this.tematicaPareja + '/' + i + '.png" alt="imagen' + i + '"></img>');
+                arrayParejas.push('<img src="imagenes/' + this.tematicaPareja + '/' + i + '.png" alt="imagen' + i + '"></img>');
+                arrayParejas.push('<img src="imagenes/' + this.tematicaPareja + '/' + i + '.png" alt="imagen' + i + '"></img>');
             }
         }
-        return this.arrayParejas;
+        this.desordenarArray(arrayParejas);
+        return arrayParejas;
     }
 
-    // Devuelve un array ("tablero") desordenado aleatoriamente (fuente W3Schools.com).
-    desordenarArray() {
-        return this.arrayDeParejas().sort(function () {
+    // Devuelve un array ("tablero") desordenado aleatoriamente (fuente W3Schools.com). 
+    // Esta función devuelve un número aleatorio entre 0 (incluido) y 1 (excluido), por lo que al restarle 0.5 se generan número positivos y 
+    // negativos de forma aleatoria (entre -0.5 y 0.49999). De esta manera la función reoordena el array colocando un elemento delante o detrás 
+    // de otro de forma aleatoria.
+    desordenarArray(array) {
+        return array.sort(function () {
             return 0.5 - Math.random()
         });
     }
@@ -156,12 +171,13 @@ class Memory extends Tablero {
     // Introduce el valor de cada posición del array con las url de las imágenes en el tablero.
     colocarParejas() {
 
+        let arrayConParejas = this.arrayDeParejas();
         let contador = 0;
 
         for (let i = 0; i < this.filas; i++) {
             for (let j = 0; j < this.columnas; j++) {
-                this.tablero[i][j] = this.arrayParejas[contador];
-                contador++
+                this.tablero[i][j] = arrayConParejas[contador];
+                contador++;
             }
         }
         return this.tablero;
@@ -170,7 +186,7 @@ class Memory extends Tablero {
     // Pinta el tablero definitivo en pantalla.
     pintarTableroEnPantalla() {
 
-        this.colocarParejas();
+        let tableroConParejas = this.colocarParejas();
 
         document.write('<h1>MEMORY</h1>');
         document.write('<table class="tabla">');
@@ -178,7 +194,7 @@ class Memory extends Tablero {
         for (let i = 0; i < this.filas; i++) {
             document.write('<tr>');
             for (let j = 0; j < this.columnas; j++) {
-                document.write('<td><div class="fichas">' + this.colocarParejas()[i][j] + '</div></td>');
+                document.write('<td><div class="fichas">' + tableroConParejas[i][j] + '</div></td>');
             }
             document.write('</tr>');
         }
@@ -186,11 +202,5 @@ class Memory extends Tablero {
     }
 }
 
-// Crea el efecto de entrada para la entrada
-function entradaTabla() {
-    const selector = document.querySelector('.tabla')
-    selector.classList.add('magictime', 'vanishIn')
-}
-setInterval(entradaTabla, 4000);
 
-const tablero1 = new Memory(maxfilas, maxColumnas, this.tematicaPareja)
+const juego = new Juego();
